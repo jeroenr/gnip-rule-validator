ce510e1081acef2267de96ad30cfe9fa231c3355
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2016-02-04 11:45:27 +0100

    Refactor and rename

--- src/main/scala/GnipRuleValidator.scala
+++ src/main/scala/GnipRuleValidator.scala
@@ -9,14 +9,13 @@ import scala.util.parsing.combinator._
 /**
  * Created by jero on 26-1-16.
  */
-object GnipRuleParser extends RegexParsers {
+object GnipRuleValidator extends RegexParsers {
   val OPERATORS = Source.fromInputStream(getClass.getResourceAsStream("/operators")).getLines.toSeq
   val STOP_WORDS = Source.fromInputStream(getClass.getResourceAsStream("/stopwords")).getLines.toSeq
 
   private val keyword = """[\w#@][\w!%&\\'*+-\./;<=>?,#@]*""".r ||| OPERATORS.map(_ ~ opt(""":[\w]+""".r)).reduceLeft(_ | _)
   private val maybeNegatedKeyword = opt("-") ~ keyword
   private val negatedKeyword = "-" ~ keyword
-  private val negatedKeywords = negatedKeyword+
 
   private val quotedKeyword = opt("-") ~ "\"" ~ (maybeNegatedKeyword+) ~ "\"" ~ opt("~[0-9]".r)
   private val negatedQuotedKeywords = "-" ~ quotedKeyword
@@ -31,11 +30,10 @@ object GnipRuleParser extends RegexParsers {
 
   private def keywordsInParentheses = "(" ~ gnipKeywordPhrase ~ ")"
   private def maybeNegatedKeywordsInParentheses = opt("-") ~ keywordsInParentheses
-  private def negatedInParentheses = ("-" ~ keywordsInParentheses)+
 
-  private def gnipKeywordPhrase: GnipRuleParser.Parser[_] = (singleKeyword ||| multipleKeywords | maybeNegatedKeywordsInParentheses)+
+  private def gnipKeywordPhrase: GnipRuleValidator.Parser[_] = (singleKeyword ||| multipleKeywords | maybeNegatedKeywordsInParentheses)+
 
-  private def guards = guard(not(negatedQuotedKeywords)) ~ guard(not(negatedKeywords)) ~ guard(not(negatedInParentheses))
+  private def guards = guard(not(negatedQuotedKeywords)) ~ guard(not(negatedKeyword+)) ~ guard(not(("-" ~ keywordsInParentheses)+))
 
   def apply(rule: String) = parse(phrase(guards ~ gnipKeywordPhrase), rule) match {
     case Success(matched, x) => scala.util.Success(matched)
---
+++ src/test/scala/GnipRuleValidatorSpec.scala
@@ -0,0 +1,178 @@
+
+import org.scalatest._
+
+class GnipRuleValidatorSpec extends WordSpec with MustMatchers with TryValues {
+
+  "Gnip rule parser" should {
+    "accept single character" in {
+      GnipRuleValidator("h").success
+    }
+    "accept single word" in {
+      GnipRuleValidator("hello").success
+    }
+    "accept upcasing word" in {
+      GnipRuleValidator("helLo WOrlD").success
+    }
+    "accept two words" in {
+      GnipRuleValidator("hello world").success
+    }
+    "accept special characters in word" in {
+      GnipRuleValidator("hello!%&\'*+-./;<=>?,#@world").success
+    }
+    "accept single hashtag" in {
+      GnipRuleValidator("#yolo").success
+    }
+    "not accept special characters in beginning of word" in {
+      GnipRuleValidator("!hello").failure
+    }
+    "accept multiple words" in {
+      GnipRuleValidator("hello? beautiful world!").success
+    }
+    "accept multiple words with negation" in {
+      GnipRuleValidator("hello? -beautiful world! -foo -bar bla -lol").success
+    }
+    "accept quoted word" in {
+      GnipRuleValidator("\"hello\"").success
+    }
+    "accept quoted words" in {
+      GnipRuleValidator("\"hello world!\"").success
+    }
+    "accept quoted negated words" in {
+      GnipRuleValidator("\"hello -world!\"").success
+    }
+    "accept negated quoted words" in {
+      GnipRuleValidator("bla -\"hello world!\"").success
+    }
+    "NOT accept negated quoted words in start position" in {
+      GnipRuleValidator("-\"hello world!\"").failure
+    }
+    "NOT accept only negated quoted words" in {
+      GnipRuleValidator("-\"hello world!\" -\"bye world!\"").failure
+    }
+    "accept all combinations of optional negation and quoted words" in {
+      GnipRuleValidator("\"hello world?\" bla -bla \"lol!\" bla").success
+    }
+    "NOT accept single negated word" in {
+      GnipRuleValidator("-hello").failure
+    }
+    "NOT accept only negated words" in {
+      GnipRuleValidator("-hello -world").failure
+    }
+    "NOT accept unfinished quotes" in {
+      GnipRuleValidator("\"-hello world\" bla \"lol bla bla").failure
+    }
+    "NOT accept empty string" in {
+      GnipRuleValidator("").failure
+    }
+    "NOT accept single stop word" in {
+      GnipRuleValidator("the").failure
+    }
+    "NOT accept single stop word 2" in {
+      GnipRuleValidator("at").failure
+    }
+    "accept stop word combined with non stop word" in {
+      GnipRuleValidator("the boat").success
+    }
+    "NOT accept only stop words" in {
+      GnipRuleValidator("a an and at but by com from http https if in is it its me my or rt the this to too via we www you").failure
+    }
+    "accept group" in {
+      GnipRuleValidator("(the boat)").success
+    }
+    "accept groups" in {
+      GnipRuleValidator("(the boat) (the other boat)").success
+    }
+    "accept nested groups" in {
+      GnipRuleValidator("((bla bla))").success
+    }
+    "accept nested groups with terms before" in {
+      GnipRuleValidator("(the boat (bla bla))").success
+    }
+    "accept nested groups with terms after" in {
+      GnipRuleValidator("((bla bla) lol lol)").success
+    }
+    "accept nested groups with terms before AND after" in {
+      GnipRuleValidator("(lol lol (\"bla\" bla) -lol lol)").success
+    }
+    "accept groups combined with non-groups" in {
+      GnipRuleValidator("the boat (bla bla)").success
+    }
+    "accept negated groups" in {
+      GnipRuleValidator("the boat -(bla bla)").success
+    }
+    "NOT accept negated group in start position" in {
+      GnipRuleValidator("-(bla bla)").failure
+    }
+    "NOT accept only negated groups" in {
+      GnipRuleValidator("-(bla bla) -(lol lol)").failure
+    }
+    "accept quoted keywords in groups" in {
+      GnipRuleValidator("(\"bla\" \"bla\")").success
+    }
+    "NOT accept unclosed groups" in {
+      GnipRuleValidator("(hello (world) bla").failed
+    }
+    "accept single powertrack operator" in {
+      GnipRuleValidator("lang:EN").success
+    }
+    "NOT accept invalid use of powertrack operator" in {
+      GnipRuleValidator("lang:").failed
+    }
+    "accept proximity operator" in {
+      GnipRuleValidator("\"happy birthday\"~3").success
+    }
+    "accept powertrack operator with terms before" in {
+      GnipRuleValidator("bla lang:en").success
+    }
+    "accept powertrack operator with terms after" in {
+      GnipRuleValidator("lang:en bla").success
+    }
+    "accept powertrack operator with terms before AND after" in {
+      GnipRuleValidator("bla lang:en bla").success
+    }
+    "accept powertrack operator with terms in parentheses before" in {
+      GnipRuleValidator("(bla bla) lang:en").success
+    }
+    "accept powertrack operator with terms in parentheses after" in {
+      GnipRuleValidator("lang:en (bla bla)").success
+    }
+    "accept powertrack operator with terms in parentheses before AND after" in {
+      GnipRuleValidator("(bla bla) lang:en (bla bla)").success
+    }
+    "accept negated powertrack operator with terms after" in {
+      GnipRuleValidator("-lang:en bla").success
+    }
+    "accept negated powertrack operator with terms before" in {
+      GnipRuleValidator("bla -lang:en").success
+    }
+    "NOT accept only negated powertrack operators" in {
+      GnipRuleValidator("-lang:en -contains:lol").failure
+    }
+    "accept negated powertrack operator with terms before AND after" in {
+      GnipRuleValidator("bla -lang:en bla").success
+    }
+    "accept multiple powertrack operators" in {
+      GnipRuleValidator("lang:en has:links from:8744 contains:help url_contains:foo").success
+    }
+    "accept combination of all" in {
+      GnipRuleValidator("(gnip OR from:688583 OR @gnip) (\"powertrack -operators\" OR \"streaming code\"~4) contains:help -lang:en").success
+    }
+    "NOT accept OR missing terms after" in {
+      GnipRuleValidator("this OR").failed // why does this pass?
+    }
+    "NOT accept OR missing terms before" in {
+      GnipRuleValidator("OR bla").failed // why does this pass?
+    }
+    "NOT accept negated OR" in {
+      GnipRuleValidator("this OR -that").failed // why does this pass?
+    }
+    "accept OR" in {
+      GnipRuleValidator("this OR that").success
+    }
+
+    //    "accept full syntax" in {
+    //      GnipRuleParser("(gnip OR from:688583 OR @gnip) (\"powertrack operators\" OR \"streaming code\"~4) contains:help bio_contains:developer has:links url_contains:github source:web (friends_count:1 OR followers_count:2000 OR listed_count:500 OR statuses_count:1000 OR is:verified OR klout_score:50) (country_code:US OR bio_location:CO OR bio_location_contains:Boulder OR time_zone:\"Mountain Time (US & Canada)\") -is_retweet (lang:en OR twitter_lang:en)").success
+    //    }
+  }
+
+}
